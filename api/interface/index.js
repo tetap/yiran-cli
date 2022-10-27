ffaa7556b2cf5dbb2b8769ecd4cc6fe82d2a9adc
zyc <239559238@qq.com>
2022-10-27 16:36:31 +0800

    ✨ feat: 1

--- api/interface/index.js
+++ api/interface/index.js
@@ -3,7 +3,7 @@ const axios = require("../api.request");
 
 const getRepoList = (params) => {
   return axios.request({
-    url: "https://api.github.com/users/yirandidi/repos",
+    url: "https://api.github.com/users/tetap/repos",
     params,
     method: "get",
   });
